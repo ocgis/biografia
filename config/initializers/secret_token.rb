188f3050fa6b3416a0afd493dea87816dc0b8139
Christer Gustavsson <cg@nocrew.org>
2014-03-07 14:27:56 +0100

    Started new application to migrate legacy code into.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biografia::Application.config.secret_key_base = '5de9164213e57219d931df9baa84f8a5077f842c9e54f7c20c7fb2683bc5ee65eed729c1f1283c047c47d80252a8810e953a4b73e145e2f25f5d5dd333fa9dc8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Biografia::Application.config.session_store :cookie_store, key: '_biografia_session'
