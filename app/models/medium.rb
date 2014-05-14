b19d51a289f640e89f7eb063779c834dce59b27a
Christer Gustavsson <cg@nocrew.org>
2014-05-14 12:40:28 +0200

    Only update cached images/thumbnails if nonexistent or old.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -20,12 +20,14 @@ class Medium < ActiveRecord::Base
       if mime_type[0].media_type == "image"
         thumb = File.join('cache', 'thumbnails', file_name + '.jpg')
         thumb_full = Rails.public_path.join(thumb)
-        thumb_full_dir = File.dirname(thumb_full)
-        FileUtils.mkdir_p(thumb_full_dir)
-        image = Magick::Image.read(file_full).first
-        image.format = "JPG"
-        image.change_geometry!("110X110") { |cols, rows| image.thumbnail! cols, rows }
-        image.write(thumb_full)
+        if (not File.exist?(thumb_full)) or (File.mtime(thumb_full) < File.mtime(file_full))
+          thumb_full_dir = File.dirname(thumb_full)
+          FileUtils.mkdir_p(thumb_full_dir)
+          image = Magick::Image.read(file_full).first
+          image.format = "JPG"
+          image.change_geometry!("110X110") { |cols, rows| image.thumbnail! cols, rows }
+          image.write(thumb_full)
+        end
         return thumb
       end
     end
@@ -37,14 +39,16 @@ class Medium < ActiveRecord::Base
     file_full = Rails.public_path.join(file_name)
     mime_type = MIME::Types.type_for(file_full.to_s)
     if mime_type.length == 1
-      if mime_type[0].media_type != "image/jpeg"
+      if mime_type[0].sub_type != "jpeg"
         fullsize = File.join('cache', 'fullsize', file_name + '.jpg')
         fullsize_full = Rails.public_path.join(fullsize)
-        fullsize_full_dir = File.dirname(fullsize_full)
-        FileUtils.mkdir_p(fullsize_full_dir)
-        image = Magick::Image.read(file_full).first
-        image.format = "JPG"
-        image.write(fullsize_full)
+        if (not File.exist?(fullsize_full)) or (File.mtime(fullsize_full) < File.mtime(file_full))
+          fullsize_full_dir = File.dirname(fullsize_full)
+          FileUtils.mkdir_p(fullsize_full_dir)
+          image = Magick::Image.read(file_full).first
+          image.format = "JPG"
+          image.write(fullsize_full)
+        end
         return fullsize
       end
     end
