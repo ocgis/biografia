eeeb5fadc65b9d8ccf7c86305d989af76fb54149
Christer Gustavsson <cg@nocrew.org>
2014-11-25 21:19:25 +0100

    Convenience script to clean database except for users.

---
+++ scripts/clean_db.rb
@@ -0,0 +1,10 @@
+# Remove all but users, transfers and media from database
+Address.destroy_all
+EventDate.destroy_all
+Event.destroy_all
+Note.destroy_all
+Person.destroy_all
+PositionInPicture.destroy_all
+Reference.destroy_all
+Relationship.destroy_all
+
