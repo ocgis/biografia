1b6bc6a4a390662b8a76a689bf4d17e42f46747d
Christer Gustavsson <cg@nocrew.org>
2015-01-27 21:55:58 +0100

    Override application controller's version of new action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
- skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
 
- def create
+  def new
+  end
+ 
+  def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_url
