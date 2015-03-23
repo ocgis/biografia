5833e8594358fe4936faa59009ce8ef47cbee201
Christer Gustavsson <cg@nocrew.org>
2015-03-23 21:10:04 +0100

    Added rendering functions person_link_to and person_show_family.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,5 +1,23 @@
 module PeopleHelper
 
+  def person_link_to(person)
+    return link_to person.long_name, :controller => person.controller, :action => 'show', :id => person.id
+  end
+
+  def person_show_family(person)
+    html = "<table>"
+    person.find_family_members.each do |family_member|
+      html << "<tr>"
+      html << "<td>"
+      html << person_link_to(family_member.object)
+      html << "</td>"
+      html << "<td>#{family_member.role}</td>"
+      html << "</tr>"
+    end
+    html << "</table>"
+    return html.html_safe
+  end
+
   def ancestry_table(ancestry, options={:depth => 2, top => false})
     if ancestry == nil
       return ""
