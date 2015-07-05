9ac781db8c87010a4dd8be1795f644c603bee290
Christer Gustavsson <cg@nocrew.org>
2015-07-05 20:33:50 +0200

    - Added google config, for embedded maps.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:ocgis/biografia.git'
 
 set :deploy_to, '/home/rails/biografia'
 
-set :linked_files, %w{config/database.yml config/initializers/facebook.rb}
+set :linked_files, %w{config/database.yml config/initializers/facebook.rb config/initializers/google.rb}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system, protected}
 
 namespace :deploy do
