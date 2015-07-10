c763490bf01b517b0c6c5af119aca9221ef04f43
Christer Gustavsson <cg@nocrew.org>
2015-07-10 20:46:39 +0200

    - Filter parent object away when presenting events.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,17 +7,20 @@ module EventsHelper
 
       concat(application_attach_modifier(object, options) do
                parts = []
-               unless related_objects[:event_dates].length == 0
-                 parts.append(related_objects[:event_dates].collect{|o| link_to(o.one_line, o)}.join(" "))
+               event_dates = related_objects[:event_dates].reject{|o| o.object_name == parent.object_name }
+               unless event_dates.length == 0
+                 parts.append(event_dates.collect{|o| link_to(o.one_line, o)}.join(" "))
                end
                parts.append(link_to(object.name, :controller => object.controller, :action => 'show', :id => object.id))
-               unless related_objects[:people].length == 0
+               people = related_objects[:people].reject{|o| o.object_name == parent.object_name }
+               unless people.length == 0
                  parts.append("med")
-                 parts.append(related_objects[:people].collect{|o| link_to(o.one_line, o)}.join(" "))
+                 parts.append(people.collect{|o| link_to(o.one_line, o)}.join(" "))
                end
-               unless related_objects[:addresses].length == 0
+               addresses = related_objects[:addresses].reject{|a| a.object_name == parent.object_name }
+               unless addresses.length == 0
                  parts.append("vid")
-                 parts.append(related_objects[:addresses].collect{|o| link_to(o.one_line, o)}.join(" "))
+                 parts.append(addresses.collect{|o| link_to(o.one_line, o)}.join(" "))
                end
                concat(parts.join(" ").html_safe)
              end)
