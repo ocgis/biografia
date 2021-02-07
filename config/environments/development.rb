670390d39a4e5e8e68bbec8e36391b786fbcc118
Christer Gustavsson <cg@nocrew.org>
2021-02-07 07:25:29 +0100

    Fix rails assets:precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,11 +22,6 @@ Biografia::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
   config.public_path = File.join(Pathname.new(::Rails.root).realpath.to_s, 'public')
   config.protected_path = File.join(Pathname.new(::Rails.root).realpath.to_s, 'protected')
   config.cache_path = File.join(config.protected_path, 'cache')
