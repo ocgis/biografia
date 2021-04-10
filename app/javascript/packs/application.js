c88c14464c918125e48936cd7dcdee90ef179f20
Christer Gustavsson <cg@nocrew.org>
2021-04-10 06:42:38 +0200

    Added UJS to make legacy code work better.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,19 +6,26 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'stylesheets/application';
+import 'src/dropdownmenu';
+import 'src/global';
+import Rails from '@rails/ujs';
 
 // Copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+const images = require.context('../images', true);
+const imagePath = (name) => images(name, true);
 
-import "stylesheets/application"
+Rails.start();
 
-require("jquery-ui")
-require("jquery-jcrop")
-require("imagesloaded")
+// Support component names relative to this directory:
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
 
-import "src/dropdownmenu.js"
-import "src/global.js"
+ReactRailsUJS.useContext(componentRequireContext);
+
+require('jquery-ui');
+require('jquery-jcrop');
+require('imagesloaded');
