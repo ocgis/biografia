b65e10ff6347cfc89886c8452e618625455461df
Christer Gustavsson <cg@nocrew.org>
2021-04-16 09:33:40 +0200

    Corrected lint warnings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 set :application, 'biografia'
 set :repo_url, 'git@github.com:ocgis/biografia.git'
 
-set :linked_files, %w{config/database.yml config/initializers/facebook.rb config/initializers/google.rb}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system, protected}
+set :linked_files, %w[config/database.yml config/initializers/facebook.rb config/initializers/google.rb]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system protected]
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
