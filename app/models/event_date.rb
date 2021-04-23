b2c502e0e2da7b676706f87227e85db0cc173f36
Christer Gustavsson <cg@nocrew.org>
2021-04-23 20:28:28 +0200

    Fixed eslint warnings.

--- app/models/event_date.rb
+++ app/models/event_date.rb
@@ -1,68 +1,72 @@
+# frozen_string_literal: true
+
+# This is the event date model
 class EventDate < ActiveRecord::Base
   has_paper_trail
 
   extend CommonClassMethods
   include CommonInstanceMethods
-  
+
   def controller
-    return "event_dates"
+    'event_dates'
   end
 
   def one_line
-    return get_date
+    get_date
   end
 
   def get_date
-    if self.mask.nil?
-      return self.date.to_s
-    elsif self.mask == "YYYY"
-      return self.date.strftime("%Y")
-    elsif self.mask == "YYYY-MM"
-      return self.date.strftime("%Y-%m")
-    elsif self.mask == "YYYY-MM-DD"
-      return self.date.strftime("%Y-%m-%d")
-    elsif self.mask == "YYYY-MM-DD hh:mm"
-      return self.date.strftime("%Y-%m-%d %H:%M")
-    elsif self.mask == "YYYY-MM-DD hh:mm:ss"
-      return self.date.strftime("%Y-%m-%d %H:%M:%S")
+    return date.to_s if mask.nil?
+
+    case mask
+    when 'YYYY'
+      date.strftime('%Y')
+    when 'YYYY-MM'
+      date.strftime('%Y-%m')
+    when 'YYYY-MM-DD'
+      date.strftime('%Y-%m-%d')
+    when 'YYYY-MM-DD hh:mm'
+      date.strftime('%Y-%m-%d %H:%M')
+    when 'YYYY-MM-DD hh:mm:ss'
+      date.strftime('%Y-%m-%d %H:%M:%S')
     else
-      return "Unknown mask #{self.mask} #{self.date.to_s}"
+      "Unknown mask #{mask} #{date}"
     end
   end
-  
+
   def set_date(dstr)
     m = dstr.match(/^(\d\d\d\d)-(\d\d)-(\d\d)$/)
-    if not m.nil?
+    unless m.nil?
       self.date = DateTime.new(m[1].to_i, m[2].to_i, m[3].to_i)
-      self.mask = "YYYY-MM-DD"
+      self.mask = 'YYYY-MM-DD'
       return true
     end
 
     m = dstr.match(/^(\d\d\d\d)-(\d\d)$/)
-    if not m.nil?
+    unless m.nil?
       self.date = DateTime.new(m[1].to_i, m[2].to_i)
-      self.mask = "YYYY-MM"
+      self.mask = 'YYYY-MM'
       return true
     end
 
     m = dstr.match(/^(\d\d\d\d)$/)
-    if not m.nil?
+    unless m.nil?
       self.date = DateTime.new(m[1].to_i)
-      self.mask = "YYYY"
+      self.mask = 'YYYY'
       return true
     end
 
     m = dstr.match(/^(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d)$/)
-    if not m.nil?
+    unless m.nil?
       self.date = DateTime.new(m[1].to_i, m[2].to_i, m[3].to_i, m[4].to_i, m[5].to_i)
-      self.mask = "YYYY-MM-DD hh:mm"
+      self.mask = 'YYYY-MM-DD hh:mm'
       return true
     end
 
     m = dstr.match(/^(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)$/)
-    if not m.nil?
+    unless m.nil?
       self.date = DateTime.new(m[1].to_i, m[2].to_i, m[3].to_i, m[4].to_i, m[5].to_i, m[6].to_i)
-      self.mask = "YYYY-MM-DD hh:mm:ss"
+      self.mask = 'YYYY-MM-DD hh:mm:ss'
       return true
     end
 
