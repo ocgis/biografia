099b860dc08a27e9b0b69f7d64271a79b69bd6a1
Christer Gustavsson <cg@nocrew.org>
2021-04-23 20:15:07 +0200

    Fixed eslint warnings.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,7 @@
-module EventsHelper
+# frozen_string_literal: true
 
+# Helper functions for the events views
+module EventsHelper
   def events_showlimitedp(object, options)
     capture do
       parent = object.get_extra(:parent)
@@ -7,24 +9,23 @@ module EventsHelper
 
       concat(application_attach_modifier(object, options) do
                parts = []
-               event_dates = related_objects[:event_dates].reject{|o| o.object_name == parent.object_name }
-               unless event_dates.length == 0
-                 parts.append(event_dates.collect{|o| link_to(o.decorate.one_line, o)}.join(" "))
+               event_dates = related_objects[:event_dates].reject { |o| o.object_name == parent.object_name }
+               unless event_dates.length.zero?
+                 parts.append(event_dates.collect { |o| link_to(o.decorate.one_line, o) }.join(' '))
                end
-               parts.append(link_to(object.name, :controller => object.controller, :action => 'show', :id => object.id))
-               people = related_objects[:people].reject{|o| o.object_name == parent.object_name }
-               unless people.length == 0
-                 parts.append("med")
-                 parts.append(people.collect{|o| link_to(o.decorate.one_line, o)}.join(" "))
+               parts.append(link_to(object.name, controller: object.controller, action: 'show', id: object.id))
+               people = related_objects[:people].reject { |o| o.object_name == parent.object_name }
+               unless people.length.zero?
+                 parts.append('med')
+                 parts.append(people.collect { |o| link_to(o.decorate.one_line, o) }.join(' '))
                end
-               addresses = related_objects[:addresses].reject{|a| a.object_name == parent.object_name }
-               unless addresses.length == 0
-                 parts.append("vid")
-                 parts.append(addresses.collect{|o| link_to(o.decorate.one_line, o)}.join(" "))
+               addresses = related_objects[:addresses].reject { |a| a.object_name == parent.object_name }
+               unless addresses.length.zero?
+                 parts.append('vid')
+                 parts.append(addresses.collect { |o| link_to(o.decorate.one_line, o) }.join(' '))
                end
-               concat(parts.join(" ").html_safe)
+               concat(parts.join(' ').html_safe)
              end)
     end
   end
-
 end
