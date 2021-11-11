624f5249aae72d6ff2b49e470265a568cfb8a702
Christer Gustavsson <cg@nocrew.org>
2021-11-11 15:21:03 +0100

    Fixed issue with depracated function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,39 @@
-# -*- coding: utf-8 -*-
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+# Users controller
+class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
     @user = User.find(params.require(:id))
   end
-  
+
   def edit
     @user = User.find(params.require(:id))
     puts @user.inspect
   end
-  
+
   def update
     id = params.require(:id)
     user = User.find(id)
-    user.update_attributes(user_params)
+    user.update(user_params)
     roles = params.require(:user).require(:roles)
     user.roles = []
     roles.each do |role|
       user.roles << role.to_sym
     end
     user.save
-    redirect_to :action => :show, :id => id
+    redirect_to action: :show, id: id
   end
 
   protected
 
   def all_objects
-    return User.all
+    User.all
   end
 
   def index_title
-    return "Alla användare"
+    'Alla användare'
   end
 
   private
@@ -40,5 +41,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :home_object_name)
   end
-
 end
