4611838fa522ea900599b06f955b8e58fa8b6ead
Christer Gustavsson <cg@nocrew.org>
2021-11-21 06:48:08 +0100

    Refactored and fixed problems with EXIF handling.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -59,92 +59,13 @@ class Medium < ActiveRecord::Base
     file_full
   end
 
-  def extra_info
-    extra_info = {}
-    full_file_name = File.join(Biografia::Application.config.protected_path, file_name)
-    file_type = MIME::Types.type_for(full_file_name).first.content_type
-    if (file_type == 'image/jpeg') || (file_type == 'image/tiff')
-      e = Exiftool.new(full_file_name)
-      extra_info = extra_info.merge(e.to_hash)
-    end
-    extra_info
-  end
-
   def handle_extra_info
-    extra_info = self.extra_info
-
-    if extra_info.key?(:image_description)
-      note = Note.create_save(note: extra_info[:image_description])
-      add_reference(note)
-    end
-
-    if extra_info.key?(:date_time_original)
-      date_time = extra_info[:date_time_original]
-      date_time[4] = '-'
-      date_time[7] = '-'
-      event_date = EventDate.new
-      event_date.set_date(date_time)
-
-      raise StandardError, "Could not save EventDate with date #{event_date}" unless event_date.save
-
-      add_reference(event_date, role: 'Date')
-    end
-
-    location = get_location(extra_info)
-    unless location.nil?
-      parts = location.split(',')
-      address = Address.create_save(latitude: parts[0].to_f, longitude: parts[1].to_f)
-      add_reference(address, role: 'Position')
-    end
-
-    device_attrs = {}
-    device_attrs[:make] = extra_info[:make] if extra_info.key?(:make)
-    device_attrs[:model] = extra_info[:model] if extra_info.key?(:model)
-    device_attrs[:serial] = extra_info[:internal_serial_number] if extra_info.key?(:internal_serial_number)
-    if extra_info.key?(:file_source) && extra_info[:file_source] == 'Digital Camera'
-      device_attrs[:kind] = 'Camera'
-      device_role = 'Camera'
-    end
-
-    device_attrs[:serial] = extra_info[:internal_serial_number] if extra_info.key?(:internal_serial_number)
-
-    if device_attrs.length.positive?
-      things = Thing.where(device_attrs)
-
-      case things.length
-      when 1
-        thing = things.first
-      when 0
-        thing = Thing.create_save(device_attrs)
-      else
-        raise StandardError, "Found several matching things: #{things.pretty_inspect}"
-      end
-      add_reference(thing, role: device_role)
-    end
-
-    return unless extra_info.key?(:lens_type)
-
-    lens_attrs = { kind: 'Camera Lens',
-                   model: extra_info[:lens_type] }
-    things = Thing.where(lens_attrs)
-    case things.length
-    when 1
-      thing = things.first
-    when 0
-      thing = Thing.create_save(lens_attrs)
-    else
-      raise StandardError, "Found several matching things: #{things.pretty_inspect}"
-    end
-    add_reference(thing, role: 'CameraLens')
-  end
-
-  def get_location(extra_info)
-    if extra_info.key?(:gps_latitude) && extra_info.key?(:gps_latitude_ref) &&
-       extra_info.key?(:gps_longitude) && extra_info.key?(:gps_longitude_ref)
-      latitude = extra_info[:gps_latitude]
-      longitude = extra_info[:gps_longitude]
-      "#{latitude},#{longitude}"
-    end
+    extra_info = exif_read_info
+    exif_handle_description(extra_info)
+    exif_handle_date(extra_info)
+    exif_handle_location(extra_info)
+    exif_handle_camera(extra_info)
+    exif_handle_lens(extra_info)
   end
 
   def all_attributes
@@ -156,5 +77,87 @@ class Medium < ActiveRecord::Base
                         type_: 'Medium',
                         positions_in_object: pio_attributes
                       }).update(extras)
+  end
+
+  private
+
+  def exif_read_info
+    extra_info = {}
+    full_file_name = File.join(Biografia::Application.config.protected_path, file_name)
+    file_type = MIME::Types.type_for(full_file_name).first.content_type
+    if (file_type == 'image/jpeg') || (file_type == 'image/tiff')
+      e = Exiftool.new(full_file_name)
+      extra_info = extra_info.merge(e.to_hash)
+    end
+    extra_info
+  end
+
+  def exif_handle_description(extra_info)
+    return if extra_info[:image_description].blank?
+
+    note = Note.create_save(note: extra_info[:image_description])
+    add_reference(note)
+  end
+
+  def exif_handle_date(extra_info)
+    return if extra_info[:date_time_original].blank?
+
+    date_time = extra_info[:date_time_original]
+    date_time[4] = '-'
+    date_time[7] = '-'
+    event_date = EventDate.new
+    event_date.set_date(date_time)
+
+    raise StandardError, "Could not save EventDate with date #{event_date}" unless event_date.save
+
+    add_reference(event_date, role: 'Date')
+  end
+
+  def exif_handle_location(extra_info)
+    unless extra_info[:gps_latitude].blank? ||
+           extra_info[:gps_latitude_ref].blank? ||
+           extra_info[:gps_longitude].blank? ||
+           extra_info[:gps_longitude_ref].blank?
+      address = Address.create_save(latitude: extra_info[:gps_latitude].to_f,
+                                    longitude: extra_info[:gps_longitude].to_f)
+      add_reference(address, role: 'Position')
+    end
+  end
+
+  def exif_handle_camera(extra_info)
+    device_attrs = {}
+    device_attrs[:make] = extra_info[:make] unless extra_info[:make].blank?
+    device_attrs[:model] = extra_info[:model] unless extra_info[:model].blank?
+    device_attrs[:serial] = extra_info[:internal_serial_number] unless extra_info[:internal_serial_number].blank?
+    if extra_info[:file_source] == 'Digital Camera'
+      device_attrs[:kind] = 'Camera'
+      device_role = 'Camera'
+    end
+
+    return unless device_attrs.length.positive?
+
+    attach_thing(device_attrs, device_role)
+  end
+
+  def exif_handle_lens(extra_info)
+    return if extra_info[:lens_type].blank?
+
+    lens_attrs = { kind: 'Camera Lens',
+                   model: extra_info[:lens_type] }
+
+    attach_thing(lens_attrs, 'CameraLens')
+  end
+
+  def attach_thing(attrs, role)
+    things = Thing.where(attrs)
+    case things.length
+    when 1
+      thing = things.first
+    when 0
+      thing = Thing.create_save(attrs)
+    else
+      raise StandardError, "Found several matching things: #{things.pretty_inspect}"
+    end
+    add_reference(thing, role: role)
   end
 end
