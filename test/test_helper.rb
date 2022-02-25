7674fbf0d01c93ee50a3428c3c1af8896b58f534
Christer Gustavsson <cg@nocrew.org>
2022-02-25 13:43:49 +0100

    Added email and password since they are not allowed to be null.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,8 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def make_user(role)
-    user = User.create!()
+    user = User.create!(email: 'apa@bepa.com',
+                        password: 'sdhakhsdkhksd')
     user.roles << role
     user.save
     session[:user_id] = user.id
