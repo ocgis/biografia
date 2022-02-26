5a6e99ee80d1de0dedaf67a57ab71aecc22e7f19
Christer Gustavsson <cg@nocrew.org>
2022-02-26 15:01:42 +0100

    Updated relationships controller test.

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,45 +1,67 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionController::TestCase
+  include Warden::Test::Helpers
+  include Devise::Test::ControllerHelpers
 
-  test "should post createp" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :createp, { :relationship => { :name => 'Test name' }, :form => { :parentName => people(:person1).object_name, :topName => people(:person1).object_name } }
+  setup do
+    # https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
+    # @user = users(:admin)
+    # sign_in @user
+  end
+
+  teardown do
+    Warden.test_reset!
+  end
+
+  test 'should post createp' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :createp,
+         params: {
+           relationship: { name: 'Test name' },
+           form: { parentName: people(:person1).object_name, topName: people(:person1).object_name }
+         }
     assert_response :success
   end
 
-  test "should get edit" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :edit, { :id => relationships(:relationship1).id, :topName => people(:person1).object_name }
+  test 'should get edit' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :edit, xhr: true, params: { id: relationships(:relationship1).id, topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should get index" do
-    make_user(:watcher)
+  test 'should get index' do
+    sign_in make_user(:watcher)
     get :index
     assert_response :success
   end
 
-  test "should get newp" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :newp, { :topName => people(:person1).object_name }
+  test 'should get newp' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :newp, xhr: true, params: { topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should get show" do
-    make_user(:watcher)
-    get :show, { :id => relationships(:relationships_references).id }
+  test 'should get show' do
+    sign_in make_user(:watcher)
+    get :show, params: { id: relationships(:relationships_references).id }
     assert_response :success
   end
 
-  test "should post update" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :update, { :id => relationships(:relationship1).id, :relationship => { :name => 'Test name' }, :form => { :topName => people(:person1).object_name } }
+  test 'should post update' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :update,
+         params: {
+           id: relationships(:relationship1).id,
+           relationship: { name: 'Test name' },
+           form: { topName: people(:person1).object_name }
+         }
     assert_response :success
   end
-
 end
