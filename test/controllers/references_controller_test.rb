9229ab548984375ce25bd259ba5efcbade7ea17c
Christer Gustavsson <cg@nocrew.org>
2022-02-26 21:35:03 +0100

    Updated references controller test.

--- test/controllers/references_controller_test.rb
+++ test/controllers/references_controller_test.rb
@@ -1,49 +1,55 @@
-# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ReferencesControllerTest < ActionController::TestCase
+  include Warden::Test::Helpers
+  include Devise::Test::ControllerHelpers
 
-  test "should post connection_add" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :connection_add, { :form => { :connect1Id => people(:person1).object_name,
-                                      :connect2Id => media(:medium1).object_name,
-                                      :x => '10',
-                                      :y => '20',
-                                      :w => '30',
-                                      :h => '40' } }
+  teardown do
+    Warden.test_reset!
+  end
+
+  test 'should post connection_add' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :connection_add, params: { form: { connect1Id: people(:person1).object_name,
+                                            connect2Id: media(:medium1).object_name,
+                                            x: '10',
+                                            y: '20',
+                                            w: '30',
+                                            h: '40' } }
     assert_response :success
   end
 
-  test "should get connection_choose" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :connection_choose, { :name => people(:person1).object_name }
+  test 'should get connection_choose' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :connection_choose, xhr: true, params: { name: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should post connection_list" do
-    make_user(:watcher)
-    @request.accept = "text/javascript"
-    get :connection_list, { :q => 'hej' }
+  test 'should post connection_list' do
+    sign_in make_user(:watcher)
+    @request.accept = 'text/javascript'
+    get :connection_list, xhr: true, params: { q: 'hej' }
     assert_response :success
   end
 
-  test "should get delete" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :delete, { :referencedId => people(:person2).object_name,
-                         :id => references(:reference1).id,
-                         :topName => people(:person1).object_name }
+  test 'should get delete' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :delete, xhr: true, params: { referencedId: people(:person2).object_name,
+                                      id: references(:reference1).id,
+                                      topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should post destroy" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :destroy, { :id => references(:reference1).id,
-                     :topName => people(:person1).object_name }
+  test 'should post destroy' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :destroy, params: { id: references(:reference1).id,
+                             topName: people(:person1).object_name }
     assert_response :success
   end
-
 end
