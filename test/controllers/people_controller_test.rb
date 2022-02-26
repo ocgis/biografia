20115b29f368d825694c72c97231399fe9aec8ff
Christer Gustavsson <cg@nocrew.org>
2022-02-26 22:07:39 +0100

    Updated people controller test.

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,67 +1,89 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PeopleControllerTest < ActionController::TestCase
+  include Warden::Test::Helpers
+  include Devise::Test::ControllerHelpers
+
+  teardown do
+    Warden.test_reset!
+  end
 
   # TODO: Fix problem and enable test
-#  test "should get ancestry" do
-#    make_user(:watcher)
-#    get :ancestry, {'id' => people(:person1).id }
-#    assert_response :success
-#  end
+  #  test 'should get ancestry' do
+  #    sign_in make_user(:watcher)
+  #    get :ancestry, {'id' => people(:person1).id }
+  #    assert_response :success
+  #  end
 
-  test "should post create" do
-    make_user(:editor)
-    post :create, { :person => { :sex => 'Sex' }, :person_name_0 => { :given_name => 'Given Name', :calling_name => 'Calling name', :surname => 'Surname' } }
+  test 'should post create' do
+    sign_in make_user(:editor)
+    post :create,
+         params: {
+           person: { sex: 'Sex' },
+           person_name_0: { given_name: 'Given Name', calling_name: 'Calling name', surname: 'Surname' }
+         }
     assert_response :redirect
   end
 
-  test "should get edit" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :edit, { :id => people(:person1).id, :topName => people(:person1).object_name }
+  test 'should get edit' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :edit, xhr: true, params: { id: people(:person1).id, topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should get index" do
-    make_user(:watcher)
+  test 'should get index' do
+    sign_in make_user(:watcher)
     get :index
     assert_response :success
   end
 
-  test "should get new" do
-    make_user(:editor)
+  test 'should get new' do
+    sign_in make_user(:editor)
     get :new
     assert_response :success
   end
 
-  test "should get show" do
-    make_user(:watcher)
-    get :show, { :id => people(:people_references).id }
+  test 'should get show' do
+    sign_in make_user(:watcher)
+    get :show, params: { id: people(:people_references).id }
     assert_response :success
   end
 
-  test "should post update" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :update, { :id => people(:person1).id, :person => { :sex => 'Sex' }, :person_name_0 => { :id => person_names(:person_name1).id, :given_name => 'Given Name', :calling_name => 'Calling name', :surname => 'Surname' }, :form => { :topName => people(:person1).object_name } }
+  test 'should post update' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :update,
+         params: {
+           id: people(:person1).id,
+           person: { sex: 'Sex' },
+           person_name_0: {
+             id: person_names(:person_name1).id,
+             given_name: 'Given Name',
+             calling_name: 'Calling name',
+             surname: 'Surname'
+           },
+           form: { topName: people(:person1).object_name }
+         }
     assert_response :success
   end
 
-  test "should get delete" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :delete, { :id => people(:person2).id,
-                         :topName => people(:person1).object_name }
+  test 'should get delete' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :delete, xhr: true, params: { id: people(:person2).id,
+                                      topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should post destroy" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :destroy, { :id => people(:person2).id,
-                     :referenceId => references(:reference1).id,
-                     :topName => people(:person1).object_name }
+  test 'should post destroy' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :destroy, params: { id: people(:person2).id,
+                             referenceId: references(:reference1).id,
+                             topName: people(:person1).object_name }
     assert_response :success
   end
-
 end
