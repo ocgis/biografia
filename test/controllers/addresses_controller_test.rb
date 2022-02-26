5e782bb3552954fbf8ea6882c5a1e08617f1ca5b
Christer Gustavsson <cg@nocrew.org>
2022-02-26 22:15:44 +0100

    Updated addresses controller test.

--- test/controllers/addresses_controller_test.rb
+++ test/controllers/addresses_controller_test.rb
@@ -1,51 +1,67 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class AddressesControllerTest < ActionController::TestCase
+  include Warden::Test::Helpers
+  include Devise::Test::ControllerHelpers
 
-  test "should post createp" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :createp, { :address => { :street => 'Street', :town => 'Town', :zipcode => '12345', :parish => 'Parish', :country => 'Country' }, :form => { :parentName => people(:person1).object_name, :topName => people(:person1).object_name } }
+  teardown do
+    Warden.test_reset!
+  end
+
+  test 'should post createp' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :createp,
+         params: {
+           address: { street: 'Street', town: 'Town', zipcode: '12345', parish: 'Parish', country: 'Country' },
+           form: { parentName: people(:person1).object_name, topName: people(:person1).object_name }
+         }
     assert_response :success
   end
 
-  test "should get edit" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :edit, { :id => addresses(:address1).id, :topName => people(:person1).object_name }
+  test 'should get edit' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :edit, xhr: true, params: { id: addresses(:address1).id, topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should get index" do
-    make_user(:watcher)
+  test 'should get index' do
+    sign_in make_user(:watcher)
     get :index
     assert_response :success
   end
 
-  test "should get new" do
-    make_user(:editor)
+  test 'should get new' do
+    sign_in make_user(:editor)
     get :new
     assert_response :success
   end
 
-  test "should get newp" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    xhr :get, :newp, { :topName => people(:person1).object_name }
+  test 'should get newp' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    get :newp, xhr: true, params: { topName: people(:person1).object_name }
     assert_response :success
   end
 
-  test "should get show" do
-    make_user(:watcher)
-    get :show, {'id' => addresses(:addresses_references).id }
+  test 'should get show' do
+    sign_in make_user(:watcher)
+    get :show, params: { id: addresses(:addresses_references).id }
     assert_response :success
   end
 
-  test "should post update" do
-    make_user(:editor)
-    @request.accept = "text/javascript"
-    post :update, { :id => addresses(:address1).id, :address => { :street => 'Street', :town => 'Town', :zipcode => '12345', :parish => 'Parish', :country => 'Country' }, :form => { :topName => people(:person1).object_name } }
+  test 'should post update' do
+    sign_in make_user(:editor)
+    @request.accept = 'text/javascript'
+    post :update,
+         params: {
+           id: addresses(:address1).id,
+           address: { street: 'Street', town: 'Town', zipcode: '12345', parish: 'Parish', country: 'Country' },
+           form: { topName: people(:person1).object_name }
+         }
     assert_response :success
   end
-
 end
