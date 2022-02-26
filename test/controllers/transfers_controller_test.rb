9d4fc26202c334c2f28507db57a8eb3c28998908
Christer Gustavsson <cg@nocrew.org>
2022-02-26 21:18:30 +0100

    Updated transfers controller test.

--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -1,32 +1,36 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class TransfersControllerTest < ActionController::TestCase
-  
-  test "should get create" do
-    get :create, { :upload => { :file_name => fixture_file_upload('transfer_upload_test.txt', 'text/plain') } }
-    assert_response :redirect
-  end
+  include Warden::Test::Helpers
+  include Devise::Test::ControllerHelpers
 
-  test "should get new" do
-    make_user(:editor)
-    get :new
-    assert_response :success
-  end
-
-  test "should get show" do
-    make_user(:watcher)
-    
-    get :show, {'id' => transfers(:transfer1).id }
-    assert_response :success
-  end
-
-  def setup
+  setup do
     FileUtils.rm_rf(Biografia::Application.config.transfer_path)
     Dir.mkdir(Biografia::Application.config.transfer_path)
   end
 
-  def teardown
+  teardown do
     FileUtils.rm_rf(Biografia::Application.config.transfer_path)
-  end  
+    Warden.test_reset!
+  end
 
+  test 'should post create' do
+    sign_in make_user(:editor)
+    post :create, params: { upload: { file_name: fixture_file_upload('transfer_upload_test.txt', 'text/plain') } }
+    assert_response :redirect
+  end
+
+  test 'should get new' do
+    sign_in make_user(:editor)
+    get :new
+    assert_response :success
+  end
+
+  test 'should get show' do
+    sign_in make_user(:watcher)
+    get :show, params: { 'id' => transfers(:transfer1).id }
+    assert_response :success
+  end
 end
