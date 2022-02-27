27e4af6f739db38fe73759dbaa8b21de71efd91b
Christer Gustavsson <cg@nocrew.org>
2022-02-27 09:10:24 +0100

    Updated medium model test.

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -1,101 +1,76 @@
-# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
 require 'test_helper'
 
+# Implementation of the medium test class
 class MediumTest < ActiveSupport::TestCase
-
-  test "medium extra_info image description" do
-    medium = Medium.create_save(file_name: "medium_image_description.jpeg")
-
-    extra_info = medium.extra_info
-
-    assert extra_info[:image_description] == "Räksmörgås", "Got [#{extra_info[:image_description]}], expected [Räksmörgås]"
-  end
-
-  test "medium extra_info date_time_original" do
-    medium = Medium.create_save(file_name: "medium_date_time.jpeg")
-
-    extra_info = medium.extra_info
-
-    test_assert({ got: extra_info[:date_time_original],
-                  expected: "2015:06:16 10:42:02" })
-  end
-
-  test "medium get_location" do
-    medium = Medium.create_save(file_name: "medium_position.jpeg")
-
-    position = medium.get_location
-
-    test_assert({ got: position,
-                  expected: "57.0,11.0" })
-  end
-
-  test "medium handle_extra_info image description" do
-    medium = Medium.create_save(file_name: "medium_image_description.jpeg")
+  test 'medium handle_extra_info image description' do
+    medium = Medium.create_save(file_name: 'medium_image_description.jpeg')
 
     medium.handle_extra_info
 
     references = medium.get_references
-    referenced_objects = references.collect{ |reference| reference.other_object(medium) }
+    referenced_objects = references.collect { |reference| reference.other_object(medium) }
 
     test_assert({ got: "#{referenced_objects.length} referenced object",
-                  expected: "1 referenced object" })
+                  expected: '1 referenced object' })
     test_assert({ got: "#{referenced_objects[0].class.name} object",
-                  expected: "Note object" })
+                  expected: 'Note object' })
     test_assert({ got: referenced_objects[0][:note],
-                  expected: "Räksmörgås" })
+                  expected: 'Räksmörgås' })
   end
 
-  test "medium handle_extra_info date_time_original" do
-    medium = Medium.create_save(file_name: "medium_date_time.jpeg")
+  test 'medium handle_extra_info date_time_original' do
+    medium = Medium.create_save(file_name: 'medium_date_time.jpeg')
 
     medium.handle_extra_info
 
     references = medium.get_references
-    referenced_objects = references.collect{ |reference| reference.other_object(medium) }
+    referenced_objects = references.collect { |reference| reference.other_object(medium) }
 
     test_assert({ got: "#{referenced_objects.length} referenced object",
-                  expected: "1 referenced object" })
+                  expected: '1 referenced object' })
     test_assert({ got: "#{referenced_objects[0].class.name} object",
-                  expected: "EventDate object" })
-    test_assert({ got: referenced_objects[0].get_date(),
-                  expected: "2015-06-16 10:42:02" })
+                  expected: 'EventDate object' })
+    test_assert({ got: referenced_objects[0].get_date,
+                  expected: '2015-06-16 10:42:02' })
   end
 
-  test "medium handle_extra_info position" do
-    medium = Medium.create_save(file_name: "medium_position.jpeg")
+  test 'medium handle_extra_info position' do
+    medium = Medium.create_save(file_name: 'medium_position.jpeg')
 
     medium.handle_extra_info
 
     references = medium.get_references
-    referenced_objects = references.collect{ |reference| reference.other_object(medium) }
+    referenced_objects = references.collect { |reference| reference.other_object(medium) }
 
     test_assert({ got: "#{referenced_objects.length} referenced object",
-                  expected: "1 referenced object" })
+                  expected: '1 referenced object' })
     test_assert({ got: "#{referenced_objects[0].class.name} object",
-                  expected: "Address object" })
+                  expected: 'Address object' })
     test_assert({ got: referenced_objects[0][:latitude].to_s,
-                  expected: "57.0" })
+                  expected: '57.0' })
     test_assert({ got: referenced_objects[0][:longitude].to_s,
-                  expected: "11.0" })
+                  expected: '11.0' })
   end
 
-  test "medium handle_extra_info camera" do
-    medium = Medium.create_save(file_name: "medium_camera.jpeg")
+  test 'medium handle_extra_info camera' do
+    medium = Medium.create_save(file_name: 'medium_camera.jpeg')
     medium.handle_extra_info
 
     references = medium.get_references
-    referenced_objects = references.collect{ |reference| reference.other_object(medium) }
+    referenced_objects = references.collect { |reference| reference.other_object(medium) }
 
     test_assert({ got: "#{referenced_objects.length} referenced object",
-                  expected: "1 referenced object" })
+                  expected: '1 referenced object' })
     test_assert({ got: "#{referenced_objects[0].class.name} object",
-                  expected: "Thing object" })
+                  expected: 'Thing object' })
     test_assert({ got: referenced_objects[0][:kind],
-                  expected: "Camera" })
+                  expected: 'Camera' })
     test_assert({ got: referenced_objects[0][:make],
-                  expected: "Kjell" })
+                  expected: 'Kjell' })
     test_assert({ got: referenced_objects[0][:model],
-                  expected: "Åke" })
+                  expected: 'Åke' })
   end
 
   private
