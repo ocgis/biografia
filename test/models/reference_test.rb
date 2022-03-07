365d5850403a499d9e044d7583afbd455833913a
Christer Gustavsson <cg@nocrew.org>
2022-03-07 20:09:03 +0100

    Fixed faulty tests.

--- test/models/reference_test.rb
+++ test/models/reference_test.rb
@@ -1,44 +1,45 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ReferenceTest < ActiveSupport::TestCase
-  test "address reference" do
+  test 'address reference' do
     reference3 = Reference.find(references(:reference3).id)
     address1 = Address.find(addresses(:address1).id)
     assert reference3 == address1.get_references[0], 'Association from reference to address does not work'
   end
 
-  test "event reference" do
+  test 'event reference' do
     reference4 = Reference.find(references(:reference4).id)
     event1 = Event.find(events(:event1).id)
     assert reference4 == event1.get_references[0], 'Association from reference to event does not work'
   end
 
-  test "event date reference" do
+  test 'event date reference' do
     reference5 = Reference.find(references(:reference5).id)
     event_date1 = EventDate.find(event_dates(:event_date1).id)
     assert reference5 == event_date1.get_references[0], 'Association from reference to event date does not work'
   end
 
-
-  test "medium reference" do
+  test 'medium reference' do
     reference7 = Reference.find(references(:reference7).id)
     medium1 = Medium.find(media(:medium1).id)
     assert reference7 == medium1.get_references[0], 'Association from reference to medium does not work'
   end
 
-  test "note reference" do
+  test 'note reference' do
     reference2 = Reference.find(references(:reference2).id)
     note1 = Note.find(notes(:note1).id)
-    assert reference2 == note1.get_references[0], 'Association from reference to note does not work'
+    assert note1.get_references.includes(reference2), 'Association from reference to note does not work'
   end
 
-  test "person reference" do
+  test 'person reference' do
     reference1 = Reference.find(references(:reference1).id)
     person2 = Person.find(people(:person2).id)
-    assert reference1 == person2.get_references[0], 'Association from reference to person does not work'
+    assert person2.get_references.includes(reference1), 'Association from reference to person does not work'
   end
 
-  test "relationship reference" do
+  test 'relationship reference' do
     reference6 = Reference.find(references(:reference6).id)
     relationship1 = Relationship.find(relationships(:relationship1).id)
     assert reference6 == relationship1.get_references[0], 'Association from reference to relationship does not work'
