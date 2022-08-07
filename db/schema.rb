f43647793f56aae1abf916573c23c1eff4159591
Christer Gustavsson <cg@nocrew.org>
2022-08-07 17:26:19 +0200

    Ran rails app:update and rake db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_060358) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_07_145959) do
   create_table "addresses", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "street", limit: 80
     t.string "town", limit: 80
@@ -19,55 +18,55 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "parish", limit: 80
     t.string "country", limit: 80
     t.string "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.decimal "latitude", precision: 13, scale: 9
     t.decimal "longitude", precision: 13, scale: 9
   end
 
   create_table "event_dates", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
-    t.datetime "date"
+    t.datetime "date", precision: nil
     t.string "mask"
     t.string "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "events", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "exports", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "file_name", null: false
     t.string "content_type", null: false
     t.string "status", limit: 1024, default: "INIT"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "identities", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "imports", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "file_name", null: false
     t.string "content_type", null: false
     t.string "status", limit: 1024, default: "INIT"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "media", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.text "file_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "notes", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -75,15 +74,15 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "title"
     t.text "note"
     t.string "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "people", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "sex", limit: 1
     t.string "source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "person_names", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -92,8 +91,8 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "calling_name", limit: 80
     t.string "surname", limit: 80
     t.integer "person_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "position_in_pictures", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -102,8 +101,8 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.float "width"
     t.float "height"
     t.integer "reference_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "references", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -112,16 +111,16 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.integer "id1", null: false
     t.string "type2", null: false
     t.integer "id2", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["type1", "id1"], name: "index_references_on_type1_and_id1"
     t.index ["type2", "id2"], name: "index_references_on_type2_and_id2"
   end
 
   create_table "relationships", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.text "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "things", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -130,15 +129,15 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "make"
     t.string "model"
     t.string "serial"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "transfers", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "file_name", null: false
     t.string "content_type", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "users", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
@@ -146,16 +145,16 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "uid"
     t.string "name"
     t.string "oauth_token"
-    t.datetime "oauth_expires_at"
+    t.datetime "oauth_expires_at", precision: nil
     t.integer "roles_mask", default: 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.string "home_object_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
@@ -166,7 +165,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_060358) do
     t.string "event", null: false
     t.string "whodunnit"
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
