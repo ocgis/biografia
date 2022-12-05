8e1e72915976b6b0362df25aaf68ae9d6455ec84
Christer Gustavsson <cg@nocrew.org>
2022-12-05 19:46:20 +0100

    Changed tag medium not to exit on select, to be able to tag multiple objects.

--- app/javascript/components/TagMedium.jsx
+++ app/javascript/components/TagMedium.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AutoComplete } from 'antd';
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+import { CheckOutlined } from '@ant-design/icons';
 import { throttle } from 'throttle-debounce';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
@@ -21,6 +21,7 @@ class TagMedium extends React.Component {
         id2: null,
         position_in_pictures: null,
       },
+      selected: [],
       crop: {
         unit: '%',
       },
@@ -49,28 +50,9 @@ class TagMedium extends React.Component {
   };
 
   render() {
-    const handleResult = (result) => {
-      const { onOk } = this.props;
-
-      if (result.error == null) {
-        onOk(result);
-      } else {
-        this.setState({ error: result.error });
-      }
-    };
-
     const okButtonClicked = () => {
-      const { reference: { type2, id2, position_in_pictures } } = this.state;
-      if (type2 == null || id2 == null || position_in_pictures == null) {
-        this.setState({ error: 'Välj referens och position' });
-      } else {
-        saveData('Reference', this.state, handleResult);
-      }
-    };
-
-    const closeButtonClicked = () => {
-      const { onCancel } = this.props;
-      onCancel();
+      const { onOk } = this.props;
+      onOk({});
     };
 
     const searchObject = throttle(500, this.search);
@@ -82,35 +64,35 @@ class TagMedium extends React.Component {
       });
     };
 
-    const onSelect = (index) => {
-      const { descriptionOptions: { [index]: option }, reference } = this.state;
-      reference.type2 = option._type_;
-      reference.id2 = option.id;
-      this.setState({
-        reference,
-      });
-      okButtonClicked();
+    const handleResult = (result) => {
+      const { props } = this;
+      if (result.error == null) {
+        this.setState({
+          reference: {
+            type1: props.referFrom._type_,
+            id1: props.referFrom.id,
+            type2: null,
+            id2: null,
+            position_in_pictures: null,
+          },
+          crop: {
+            unit: '%',
+          },
+        });
+      } else {
+        this.setState({ error: result.error });
+      }
     };
 
-    const {
-      descriptionOptions, crop, error, value,
-    } = this.state;
-    const { referFrom } = this.props;
-
-    const options = descriptionOptions.map((item, index) => {
-      const ShowObject = showObject(item._type_);
-      return ({
-        value: index,
-        label: (
-          <ShowObject
-            object={item}
-            mode="oneLine"
-            currentUser={{}}
-            reload={() => {}}
-          />
-        ),
-      });
-    });
+    const onSelect = (option) => {
+      const { reference } = this.state;
+      reference.type2 = option._type_;
+      reference.id2 = option.id;
+      this.setState((prevState) => ({
+        selected: [...prevState.selected, option],
+      }));
+      saveData('Reference', this.state, handleResult);
+    };
 
     const onCropChange = (_, newCrop) => {
       const { reference } = this.state;
@@ -131,6 +113,35 @@ class TagMedium extends React.Component {
       });
     };
 
+    const {
+      descriptionOptions, crop, error, reference, selected, value,
+    } = this.state;
+    const { referFrom } = this.props;
+
+    let ignoredKeys = [`${referFrom._type_}_${referFrom.id}`];
+    Object.keys(referFrom.related).forEach((key) => {
+      ignoredKeys = ignoredKeys.concat(referFrom.related[key].map((obj) => `${obj._type_}_${obj.id}`));
+    });
+
+    ignoredKeys = ignoredKeys.concat(selected.map((x) => `${x._type_}_${x.id}`));
+
+    const filtered = descriptionOptions.filter((x) => !ignoredKeys.includes(`${x._type_}_${x.id}`));
+
+    const options = filtered.map((item, index) => {
+      const ShowObject = showObject(item._type_);
+      return ({
+        value: index,
+        label: (
+          <ShowObject
+            object={item}
+            mode="oneLine"
+            currentUser={{}}
+            reload={() => {}}
+          />
+        ),
+      });
+    });
+
     return (
       <table>
         <tbody>
@@ -143,22 +154,25 @@ class TagMedium extends React.Component {
               />
             </td>
           </tr>
-          <tr>
-            <td>
-              <AutoComplete
-                value={value}
-                style={{ width: '50ch' }}
-                options={options}
-                onSearch={(search) => searchObject(search)}
-                onChange={onChange}
-                onSelect={onSelect}
-              />
-            </td>
-          </tr>
+          { reference.position_in_pictures != null
+            && (
+              <tr>
+                <td>
+                  <AutoComplete
+                    value={value}
+                    style={{ width: '50ch' }}
+                    options={options}
+                    onSearch={(search) => searchObject(search)}
+                    onChange={onChange}
+                    onSelect={(index) => onSelect(filtered[index])}
+                    open
+                  />
+                </td>
+              </tr>
+            )}
           <tr>
             <td>
               <CheckOutlined onClick={okButtonClicked} />
-              <CloseOutlined onClick={closeButtonClicked} />
               { error }
             </td>
           </tr>
@@ -169,10 +183,10 @@ class TagMedium extends React.Component {
 }
 TagMedium.propTypes = {
   onOk: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
   referFrom: PropTypes.shape({
     _type_: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
+    related: PropTypes.shape().isRequired,
   }).isRequired,
 };
 TagMedium.defaultProps = {
