c82c530af8a0a63a4454fb384f49cabff33d2fea
Christer Gustavsson <cg@nocrew.org>
2026-04-28 17:44:44 +0200

    Refactor as function component.

--- app/javascript/components/FormAddress.jsx
+++ app/javascript/components/FormAddress.jsx
@@ -2,143 +2,127 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input } from 'antd';
 
-class FormAddress extends React.Component {
-  constructor(props) {
-    super(props);
+function FormAddress(props) {
+  const { onChange, object: address } = props;
 
-    const { object: address } = props;
-    this.state = { address: JSON.parse(JSON.stringify(address)) };
-  }
-
-  componentDidMount() {
-    const { onChange } = this.props;
-    const { address } = this.state;
-    onChange({ address });
-  }
-
-  render() {
-    const { onChange } = this.props;
-    const { address } = this.state;
-
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              Gata:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.street}
-                onChange={(event) => {
-                  address.street = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Postort:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.town}
-                onChange={(event) => {
-                  address.town = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Postnummer:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.zipcode}
-                onChange={(event) => {
-                  address.zipcode = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Församling:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.parish}
-                onChange={(event) => {
-                  address.parish = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Land:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.country}
-                onChange={(event) => {
-                  address.country = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Latitud:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.latitude}
-                onChange={(event) => {
-                  address.latitude = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Longitud:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.longitude}
-                onChange={(event) => {
-                  address.longitude = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Källa:
-            </td>
-            <td>
-              <Input
-                defaultValue={address.source}
-                onChange={(event) => {
-                  address.source = event.target.value;
-                  onChange({ address });
-                }}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            Gata:
+          </td>
+          <td aria-label="street">
+            <Input
+              value={address.street}
+              onChange={(event) => {
+                address.street = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Postort:
+          </td>
+          <td aria-label="town">
+            <Input
+              value={address.town}
+              onChange={(event) => {
+                address.town = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Postnummer:
+          </td>
+          <td aria-label="zipcode">
+            <Input
+              value={address.zipcode}
+              onChange={(event) => {
+                address.zipcode = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Församling:
+          </td>
+          <td aria-label="parish">
+            <Input
+              value={address.parish}
+              onChange={(event) => {
+                address.parish = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Land:
+          </td>
+          <td aria-label="country">
+            <Input
+              value={address.country}
+              onChange={(event) => {
+                address.country = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Latitud:
+          </td>
+          <td aria-label="latitude">
+            <Input
+              value={address.latitude}
+              onChange={(event) => {
+                address.latitude = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Longitud:
+          </td>
+          <td aria-label="longitude">
+            <Input
+              value={address.longitude}
+              onChange={(event) => {
+                address.longitude = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Källa:
+          </td>
+          <td aria-label="source">
+            <Input
+              value={address.source}
+              onChange={(event) => {
+                address.source = event.target.value;
+                onChange({ address });
+              }}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 FormAddress.propTypes = {
   onChange: PropTypes.func.isRequired,
