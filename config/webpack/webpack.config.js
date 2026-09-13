b2644f86da42c758f43045f1cfe4d41a213d2fd3
Christer Gustavsson <cg@nocrew.org>
2026-09-13 09:10:24 +0200

    Configure webpack extensions and resolve paths.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,28 +1,40 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
   entry: {
-    application: "./app/javascript/application.js"
+    application: './app/javascript/packs/application.js',
+    Page: './app/javascript/packs/Page.jsx',
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
+    filename: '[name].js',
+    // sourceMapFilename: '[file].map',
+    // chunkFormat: 'module',
     path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
+      maxChunks: 1,
+    }),
   ],
-  rules: [
-    {
-      test: /\.(js|jsx|ts|tsx|)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx|)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    alias: {
+      src: path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+      stylesheets: path.resolve(__dirname, '..', '..', 'app/javascript/stylesheets'),
+      components: path.resolve(__dirname, '..', '..', 'app/javascript/components'),
+      'jquery-ui': 'jquery-ui-dist/jquery-ui',
     },
-  ],
+  },
 };
